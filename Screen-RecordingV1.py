a1dc4b965fe07e8571a1f274c89947a142e95dce
samwpr <swpr10x@gmail.com>
2022-10-13 15:17:51 +0800

    completed

--- Screen-RecordingV1.py
+++ Screen-RecordingV1.py
@@ -1,9 +1,10 @@
-#Apple scripts: https://eastmanreference.com/complete-list-of-applescript-key-codes
-
 import os
 import time
+import datetime 
 
-#os.system("open /Applications/OBS.app")
+#Set the time when you want the recording to end below 
+targetTime = 1517 
+restIntervals = 10
 
 quick_time_player = """
 osascript -e 'tell application "System Events" to key code 23 using {command down, shift down}' 
@@ -18,7 +19,13 @@ osascript -e 'tell application "System Events" to key code 53 using {command dow
 """
 
 os.system(quick_time_player)
-time.sleep(5)
 os.system(record)
-time.sleep(5)
+
+string_currentTime = 0
+while string_currentTime < targetTime:
+    currentTime = datetime.datetime.now()
+    string_currentTime = str(currentTime.time())
+    string_currentTime = int((string_currentTime.replace(':',''))[0:4])
+    time.sleep(restIntervals) 
+    print(string_currentTime)
 os.system(stop)
